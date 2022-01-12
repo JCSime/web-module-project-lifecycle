aa1572a4abca4d2a3c5a7ee9efec5faddbbff088
John S <johnc.sime@gmail.com>
2022-01-11 19:32:49 -0500

    Moved everything out of App.js into its own file. Implimented state. MVP complete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import React from 'react';
+import User from './components/User';
+import FollowerList from './components/FollowerList';
+import axios from 'axios';
 
 class App extends React.Component {
+    state = {
+        currentUser: "JCSime",
+        user: {},
+        followers: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+        .then(resp=> {
+            this.setState({
+                ...this.state,
+                user: resp.data
+            });
+        })
+    }
+
+    componentDidUpdate (prevProps, prevState) {
+        if(this.state.user !== prevState.user) {
+            axios.get(`https://api.github.com/users/${this.state.currentUser}/followers`)
+            .then(resp=> {
+            this.setState({
+                ...this.state,
+                followers: resp.data
+                });
+            })
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            ...this.state,
+            currentUser: e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+        .then(resp=> {
+            this.setState({
+                ...this.state,
+                user: resp.data
+            });
+        })
+    }
+
     render() {
         return(
-        <div>
-            <h1>GITHUB INFO</h1>
-            <form>
-                <input placeholder="Github Handle"/>
-                <button>Search</button>
-            </form>
-            <div id="UserCard">
-                <img src="https://avatars.githubusercontent.com/u/18543621?v=4"/>
-                <a target="_blank" href="https://github.com/JCSime"><h3>John Sime</h3></a>
-                <p>(JCSime)</p>
-                <p>Total Repos: 44</p>
-                <p>Total Followers: 88</p>
-            </div>
+            <div>
+                <h1>GITHUB INFO</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="Github Handle" onChange={this.handleChange}/>
+                    <button>Search</button>
+                </form>
+                
+                <User user={this.state.user}/>
+                <FollowerList followers={this.state.followers} />
 
-            <div id="Followers">
-                <div className="follower">
-                    <img width="200px" src="https://avatars.githubusercontent.com/u/42678545?v=4"/>
-                    <a target="_blank" href="https://github.com/DatBoiLuiskrrt"><p>DatBoiLuiskrrt</p></a>
-                </div>
-                <div className="follower">
-                    <img width="200px" src="https://avatars.githubusercontent.com/u/42678545?v=4"/>
-                    <a target="_blank" href="https://github.com/DatBoiLuiskrrt"><p>DatBoiLuiskrrt</p></a>
-                </div>
-                <div className="follower">
-                    <img width="200px" src="https://avatars.githubusercontent.com/u/42678545?v=4"/>
-                    <a target="_blank" href="https://github.com/DatBoiLuiskrrt"><p>DatBoiLuiskrrt</p></a>
-                </div>
-            </div>
-        </div>
-        );
+            </div> );
     }
 }
 
